caaee8ef8ee0bacaf568a4e967f02530747e31fe
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2023-10-23 16:11:43 -0300

    Configurando pastas e componentes

---
+++ pages/page.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import styles from "../styles/HomeNoAuth.module.scss";
+
+export default function HomeNoAuth() {
+  return (
+    <>
+      <Head>
+        <title>OneBitFlix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+        <meta
+          name="description"
+          content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."
+        />
+      </Head>
+      <main></main>
+    </>
+  );
+}
