f5d25a7a8790412fa3994b2a4efcf4aa2dcf039d
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2023-10-26 16:21:44 -0300

    Criação dos elementos, do visual e também da sua responsividade - Footer, comum

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import SlideSection from "@/components/homeNoAuth/slideSection";
 import { GetStaticProps } from "next";
 import courseService, { CourseType } from "@/services/courseService";
 import { ReactNode } from "react";
+import Footer from "@/components/common/footer";
 
 interface IndexPageProps {
   children?: ReactNode;
@@ -32,6 +33,7 @@ const HomeNotAuth = function ({ course }: IndexPageProps) {
         </div>
         <CardsSection />
         <SlideSection newestCourses={course} />
+        <Footer />
       </main>
     </>
   );
