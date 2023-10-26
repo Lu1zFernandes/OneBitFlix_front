908417e15d1897c51bfa517156d926dacb3d18ca
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2023-10-26 11:01:37 -0300

    Colocando em tela o slides e ponto de retorno

---
+++ src/components/common/slideCard/index.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from "./styles.module.scss";
+import { CourseType } from "@/services/courseService";
+
+interface props {
+  course: CourseType;
+}
+
+const SlideCard = function ({ course }: props) {
+  return (
+    <>
+      <div className={styles.slide}>
+        <img
+          src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`}
+          alt={course.name}
+          className={styles.slideImg}
+        />
+        <p className={styles.slideTitle}>{course.name}</p>
+        <p className={styles.slideDescription}>{course.synopsis}</p>
+      </div>
+    </>
+  );
+};
+
+export default SlideCard;
---
+++ src/components/common/slideComponent/index.tsx
@@ -0,0 +1,36 @@
+import { CourseType } from "@/services/courseService";
+import {
+  Splide,
+  SplideSlide,
+} from "../../../../node_modules/@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+import SlideCard from "../slideCard";
+
+interface props {
+  course: CourseType[];
+}
+
+const SlideComponent = function ({ course }: props) {
+  return (
+    <>
+      <div>
+        <Splide
+          options={{
+            type: "loop",
+            perPage: 4,
+            perMove: 1,
+            pagination: false,
+          }}
+        >
+          {course?.map((course) => (
+            <SplideSlide key={course.id}>
+              <SlideCard course={course} />
+            </SplideSlide>
+          ))}
+        </Splide>
+      </div>
+    </>
+  );
+};
+
+export default SlideComponent;
