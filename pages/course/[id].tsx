0889365589e0795f39450134e4e07728984d5914
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2023-11-08 11:05:36 -0300

    Criação dos elementos da página de espisódio, do seu estilo visual e também da sua responsividade completa - Episódio

--- pages/course/[id].tsx
+++ pages/course/[id].tsx
@@ -142,7 +142,11 @@ const CoursePage = function () {
               </p>
             ) : (
               course?.episodes?.map((episode) => (
-                <EpisodeList key={episode.id} episode={episode} />
+                <EpisodeList
+                  key={episode.id}
+                  episode={episode}
+                  course={course}
+                />
               ))
             )}
           </Container>
--- src/components/episodesList/index.tsx
+++ src/components/episodesList/index.tsx
@@ -1,11 +1,15 @@
-import { EpisodeType } from "@/services/courseService";
+import { CourseType, EpisodeType } from "@/services/courseService";
 import styles from "./styles.module.scss";
+import { useRouter } from "next/router";
 
 interface props {
   episode: EpisodeType;
+  course: CourseType;
 }
 
-const EpisodeList = function ({ episode }: props) {
+const EpisodeList = function ({ episode, course }: props) {
+  const router = useRouter();
+
   const handleSecondsToMin = (totalSeconds: number) => {
     const minutes = Math.floor(totalSeconds / 60);
 
@@ -20,9 +24,13 @@ const EpisodeList = function ({ episode }: props) {
     return result;
   };
 
+  const handleEpisodePlayer = () => {
+    router.push(`/course/episode/${episode.order - 1}?courseid=${course.id}`);
+  };
+
   return (
     <>
-      <div className={styles.episodeCard}>
+      <div className={styles.episodeCard} onClick={handleEpisodePlayer}>
         <div className={styles.episodeOrderTime}>
           <p className={styles.episodeOrder}>Episódio Nº {episode.order}</p>
           <p className={styles.episodeTime}>
