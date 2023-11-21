7d84589622f57ba452c72bed13773515e5b51744
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2023-11-21 10:01:18 -0300

    Configuração

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Amplify } from "aws-amplify";
+import amplifyconfig from "../src/amplifyconfiguration.json";
+Amplify.configure(amplifyconfig);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
